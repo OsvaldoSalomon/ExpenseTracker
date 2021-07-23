78e67028d5bb8c1eec6a7326a9f75e1450a3d04c
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2021-07-23 13:26:30 -0400

    The project now filters the expenses per year, also the add new expense button has been added.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,13 +6,15 @@ function ExpenseItem(props) {
     console.log("ExpenseItem evaluated by react")
 
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,33 +6,16 @@ function ExpenseForm(props) {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
-    //Comments are for a single state that is updated depending on the previous state
-    /*const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })*/
-
     function titleChangeHandler(event) {
         setEnteredTitle(event.target.value);
-
-        /*setUserInput((prevState) => {
-            return { ...prevState, enteredTitle: event.target.value };
-        })*/
     }
 
     function amountChangeHandler(event) {
         setEnteredAmount(event.target.value);
-        /*setUserInput((prevState) => {
-            return { ...prevState, enteredAmount: event.target.value };
-        })*/
     }
 
     function dateChangeHandler(event) {
         setEnteredDate(event.target.value);
-        /*setUserInput((prevState) => {
-            return { ...prevState, enteredDate: event.target.value };
-        })*/
     }
 
     function submitHandler(event) {
@@ -82,6 +65,7 @@ function ExpenseForm(props) {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
